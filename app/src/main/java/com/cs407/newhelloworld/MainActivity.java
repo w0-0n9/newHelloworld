290aa30418d923f904287edc5320b9c7b62fd439
Jinwoong Shin <jinwoong7116@gmail.com>
2023-09-17 17:30:08 -0500

    Update

--- app/src/main/java/com/cs407/newhelloworld/MainActivity.java
+++ app/src/main/java/com/cs407/newhelloworld/MainActivity.java
@@ -3,9 +3,11 @@ package com.cs407.newhelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
